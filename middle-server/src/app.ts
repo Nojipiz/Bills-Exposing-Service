c901c2ac8f3b10c1f802854ca586e472a739d72e
Orlando Vargas <nojipiz@protonmail.com>
2022-01-23 19:45:24 +0000

    Implemented request to file server

--- middle-server/src/app.ts
+++ middle-server/src/app.ts
@@ -1,11 +1,18 @@
-import * as express from "express";
+import * as express from 'express';
+import axios from 'axios';
 
 const app = express()
+const PORT: number = 3000;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!')
+app.get('/get_bill', (req: express.Request, res: express.Response) => {
+  const billId: number = parseInt(req.header('bill_id')!);
+  const billPeriod: number = parseInt(req.header('period')!);
+  if (!billId || !billPeriod) res.header('Error', 'Not numeric values are alowed').status(400).end();
+  const query: string = `http://127.0.0.1:8080/bills/${billPeriod}/${billId}`;
+  axios.get(query).then(response => {
+  })
 })
 
 app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
+  console.log(`Express server started in port ${PORT}`)
 })
