0a462c04ebbdc95775db1eca74a0cc401ca68a92
Orlando Vargas <nojipiz@protonmail.com>
2022-01-23 19:58:15 +0000

    Added error handling in middle server

--- middle-server/src/app.ts
+++ middle-server/src/app.ts
@@ -3,14 +3,18 @@ import axios from 'axios';
 
 const app = express()
 const PORT: number = 3000;
+const FILES_SERVER_URL: String = "http://127.0.0.1:8080/bills/"
 
 app.get('/get_bill', (req: express.Request, res: express.Response) => {
   const billId: number = parseInt(req.header('bill_id')!);
   const billPeriod: number = parseInt(req.header('period')!);
-  if (!billId || !billPeriod) res.header('Error', 'Not numeric values are alowed').status(400).end();
-  const query: string = `http://127.0.0.1:8080/bills/${billPeriod}/${billId}`;
+  if (!billId || !billPeriod) res.send('Error Only numeric values are alowed').status(400);
+  const query: string = `${FILES_SERVER_URL}${billPeriod}/${billId}`;
   axios.get(query).then(response => {
-  })
+    res.status(200).send(response.data);
+  }).catch(() => {
+    res.status(503).end();
+  });
 })
 
 app.listen(3000, () => {
